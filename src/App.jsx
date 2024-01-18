d5253af9ecaf481c0093caeb172303e8421339e6
francho96 <franco.barria@usach.cl>
2024-01-18 12:48:04 -0300

    added loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import styles from './App.module.css';
+
+import logoTuna from './assets/logo-tuna_bn.png';
+import lamericanaBorder from './assets/LamericanaBorder.png';
 
 const Layout = lazy(() => import('./layout/GeneralLayout'));
 const Historia = lazy(() => import('./views/historia/Historia'));
+
+function Loading() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const delay = 3000;
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, delay);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return loading ? (
+        <div className={styles.loadingLetterLoading}>
+            <img
+                width="300px"
+                className={styles.rotateBorder}
+                src={lamericanaBorder}
+            ></img>
+            <img
+                width="130px"
+                className={styles.lamericanaLetter}
+                src={logoTuna}
+            ></img>
+        </div>
+    ) : (
+        <Layout />
+    );
+}
 
 function App() {
     return (
         <>
             <BrowserRouter>
-                <Suspense fallback={<>Cargando </>}>
+                <Suspense fallback={<></>}>
                     <Routes>
-                        <Route path="/" element={<Layout />}>
+                        <Route path="/" element={<Loading />}>
                             <Route
                                 path="historia"
                                 element={<Historia />}
